39e29a0289650762074e29f34a5b8ef67281aef9
FERNANDAFREITAS29 <fer.s.freitas29@gmail.com>
2023-09-11 19:40:28 -0300

    cneso

--- exercicio1.py
+++ exercicio1.py
@@ -6,7 +6,7 @@
 # criar a classe
 class Computador:
    
- def _init_(self, marca, memoria_ram, placa_de_video):
+ def __init__(self, marca, memoria_ram, placa_de_video):
    self.marca = marca
    self.memoria_ram = memoria_ram
    self.placa_de_video = placa_de_video
