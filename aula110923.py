3b67860c7b3bd7176193161d758441c73fc419a1
FERNANDAFREITAS29 <fer.s.freitas29@gmail.com>
2023-09-11 20:06:47 -0300

    jh

--- aula110923.py
+++ aula110923.py
@@ -6,20 +6,22 @@ Velocidade Maxima, Cor, Ligado
 
 class carro:
     def __init__(self, velMax, ligado,cor):
-        self.velmax =velMax
+        self.velmax = velMax
         self.ligado = ligado
         self.cor = cor
 
-        def Mostrar(self):
-            print('Velocidade maxima: ' + str (self.vel.Max))
-            print('cor...............: ' + self.cor)
-            estado = 'sim' if self.ligado else 'N'
-            print('ligado.............: ' + estado)
-            print('.........................')
+    def Mostrar(self):
+        print('Velocidade maxima: ' + str (self.velmax))
+        print('cor...............: ' + self.cor)
+        estado = 'sim' if self.ligado else 'N'
+        print('ligado.............: ' + estado)
+        print('.........................')
 
-c1 = Carro(200,False,'Preto')
-c2 = Carro(120,False,'Branco')
-c3 = Carro(300,False,'Vermelho')
+            
+
+c1 = carro(200,False,'Preto')
+c2 = carro(120,False,'Branco')
+c3 = carro(300,False,'Vermelho')
 c1.Mostrar()
 c2.Mostrar()
 c3.Mostrar()
