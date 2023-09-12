c70f2c2e44f29d8af24cfd0055892f4a1bc11cec
FERNANDAFREITAS29 <fer.s.freitas29@gmail.com>
2023-09-12 19:33:32 -0300

    koithjg

---
+++ aula120923.py
@@ -0,0 +1,11 @@
+# Modulos
+import math
+from math import sqrt, floor 
+
+num = int(input('digite um numero: '))
+
+raiz = math.sqrt(num)
+
+#print('a Raiz de {} e igual a {}' .format(num, raiz))
+#print('a Raiz de {} e igual a {}' .format(num, math.floor(raiz)))
+print('a Raiz de {} e igual a {}' .format(num, floor(raiz)))
