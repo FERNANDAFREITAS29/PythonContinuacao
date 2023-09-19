79a8f80a08661dbe7ffd8e053937ecf5213f7b90
FERNANDAFREITAS29 <fer.s.freitas29@gmail.com>
2023-09-19 18:55:02 -0300

    fernanda

--- items/exercFuncao.py/funcao1.py
+++ items/exercFuncao.py/funcao1.py
@@ -1,3 +1,5 @@
+'''
+
 def numero():
     print('esta função vai aceitar um numero')
 
@@ -13,3 +15,22 @@ def retornar_dobro_do_quadrado():
 numero()
 retornar_dobro()
 retornar_dobro_do_quadrado()
+
+'''
+# criar um programa que calcula a quantidade de tinta necessaria para pintar uma parede
+#o usuario devera fornecer as seguintes informações:
+# Rendimento, altura e lagura
+#o programa deve mostrar na tela a mensagem 
+# "voçe necessita de x latas de tinta"
+
+redimento = int(input('Qual e o redimento da lata? '))
+altura = int(input('qual e altura da parede? '))
+largura = int(input('qual e a largura da parede? '))
+
+# altura * lagura / rendimento = qtd de latas
+def calculo_tinta():
+    area = altura * largura
+    total = area / redimento
+    print(f'você precisa de {total} latas de tinta')
+
+calculo_tinta()
